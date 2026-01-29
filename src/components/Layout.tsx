e2caa714bd317a2b0934a7f2cdfa46f69cd75eed
poojiarc <poojimedabayina@gmail.com>
2026-01-29 12:46:37 +0530

    changes updated

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,36 +1,8 @@
-// import { ReactNode } from 'react';
-// import Header from './Header';
-// import Footer from './Footer';
-// import WhatsAppButton from './WhatsAppButton';
-// import ScrollToTop from './ScrollToTop';
-
-// interface LayoutProps {
-//   children: ReactNode;
-// }
-
-// const Layout = ({ children }: LayoutProps) => {
-//   return (
-//     <div className="min-h-screen flex flex-col">
-//       <Header />
-//       <main className="flex-1 pt-20">
-//         {children}
-//       </main>
-//       <Footer />
-//       <WhatsAppButton />
-//       <ScrollToTop />
-//     </div>
-//   );
-// };
-
-// export default Layout;
-
-
-
-import { ReactNode } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import WhatsAppButton from "./WhatsAppButton";
-import ScrollToTop from "./ScrollToTop";
+import { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import WhatsAppButton from './WhatsAppButton';
+import ScrollToTop from './ScrollToTop';
 
 interface LayoutProps {
   children: ReactNode;
@@ -40,12 +12,9 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-
-      {/* NO padding-top here */}
-      <main className="flex-1">
+      <main className="flex-1 pt-20">
         {children}
       </main>
-
       <Footer />
       <WhatsAppButton />
       <ScrollToTop />
